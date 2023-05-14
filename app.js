d888fa8f1826a06b7f2260fab759b097f462b37d
Negrea-Andrei <negrea97@yahoo.com>
2023-05-14 21:16:49 +0300

    Object prototype

--- app.js
+++ app.js
@@ -113,3 +113,41 @@ Object.prototype.hasOwnProperty('hasOwnProperty'); /*=> return true*/
 /*because prototype.hasOwnProperty returns true if the property is a direct property of the object 
 — even if the value is null or undefined. 
 The method returns false if the property is inherited, or has not been declared at all.*/
+
+/* Recommended Method for Prototypal Inheritance */
+function Person(name) {
+  this.name = name
+}
+
+Person.prototype.sayName = function() {
+  console.log(`Hello, I'm ${this.name}!`)
+}
+
+function Player(name, marker) {
+  this.name = name
+  this.marker = marker
+}
+
+Player.prototype.getMarker = function() {
+  console.log(`My marker is '${this.marker}'`)
+}
+
+// Object.getPrototypeOf(Player.prototype) should 
+// return the value of "Person.prototype" instead 
+// of "Object.prototype"
+Object.getPrototypeOf(Player.prototype) // returns Object.prototype
+
+// Now make `Player` objects inherit from `Person`
+Object.setPrototypeOf(Player.prototype, Person.prototype)
+Object.getPrototypeOf(Player.prototype) // returns Person.prototype
+
+const player3 = new Player('steve', 'X')
+const player4 = new Player('also steve', 'O')
+
+player1.sayName() // Hello, I'm steve!
+player2.sayName() // Hello, I'm also steve!
+
+player1.getMarker() // My marker is 'X'
+player2.getMarker() // My marker is 'O'
+
+console.log(Object.getPrototypeOf(Player.prototype))
