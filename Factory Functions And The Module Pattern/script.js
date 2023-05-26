b0fd1f63baf514bab8467647667259d48687644e
Negrea-Andrei <negrea97@yahoo.com>
2023-05-26 21:47:45 +0300

    Module Pattern

--- Factory Functions And The Module Pattern/script.js
+++ Factory Functions And The Module Pattern/script.js
@@ -149,12 +149,7 @@ const calculator = (() => {
   const sub = (a, b) => a - b;
   const mul = (a, b) => a * b;
   const div = (a, b) => a / b;
-  return {
-    add,
-    sub,
-    mul,
-    div,
-  };
+  return {add, sub, mul, div};
 })();
 
 calculator.add(3,5); // 8
