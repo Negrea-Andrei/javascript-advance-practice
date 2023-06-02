334ef220f9bcda3d0893cc04bb401a5d7b662252
Negrea-Andrei <negrea97@yahoo.com>
2023-06-02 17:53:40 +0300

    Setter

--- Setter and Getter/exercise.js
+++ Setter and Getter/exercise.js
@@ -3,13 +3,13 @@
 //Example
 let obj = {
     get propName() {
-      // getter, the code executed on getting obj.propName
+        // getter, the code executed on getting obj.propName
     },
-  
+
     set propName(value) {
-      // setter, the code executed on setting obj.propName = value
+        // setter, the code executed on setting obj.propName = value
     }
-  };
+};
 //The getter works when obj.propName is read.
 //The setter works when obj.propName it is assigned.
 
@@ -17,10 +17,35 @@ let obj = {
 let user = {
     name: "John",
     surname: "Smith",
-  
+
     get fullName() {
-      return `${this.name} ${this.surname}`;
+        return `${this.name} ${this.surname}`;
     }
-  };
-  
-  console.log(user.fullName); // John Smith
+};
+
+console.log(user.fullName); // John Smith
+
+//Setter
+let player = {
+    name: "John",
+    surname: "Smith",
+
+    get fullName() {
+        return `${this.name} ${this.surname}`;
+    },
+
+    set fullName(value) {
+        [this.name, this.surname] = value.split(" ");
+    }
+};
+console.log(player.name); // John
+console.log(player.surname); // Smith
+
+// set fullName is executed with the given value.
+player.fullName = "Alice Cooper";
+
+console.log(player.name); // Alice
+console.log(player.surname); // Cooper
+
+// set fullName is executed with the given value.
+console.log(player.fullName) = "Alice Cooper";
