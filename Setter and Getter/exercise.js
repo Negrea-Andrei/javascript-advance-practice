bb859e6d2daa19f0a0a3ee5407c607a4074834e2
Negrea-Andrei <negrea97@yahoo.com>
2023-06-02 17:09:54 +0300

    Getter and setter

---
+++ Setter and Getter/exercise.js
@@ -0,0 +1,12 @@
+//Setter and Getter
+//Accessor property. They are essentially functions that execute on getting and setting a value, but look like regular properties to an external code.
+//Example
+let obj = {
+    get propName() {
+      // getter, the code executed on getting obj.propName
+    },
+  
+    set propName(value) {
+      // setter, the code executed on setting obj.propName = value
+    }
+  };
