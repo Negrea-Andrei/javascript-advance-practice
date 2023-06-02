aaadff9feb3fe588e6ea294692ba5878e5a958d6
Negrea-Andrei <negrea97@yahoo.com>
2023-06-02 19:28:40 +0300

    Smarter getters/setters

--- Setter and Getter/exercise.js
+++ Setter and Getter/exercise.js
@@ -100,4 +100,22 @@ user.name = "Pete";
 alert(user.name); // Pete
 
 user.name = ""; // Name is too short...
-
+//!Using for compatibility
+//*One of the great uses of accessors is that they allow to take control over a “regular” data property at any moment by replacing it with a getter and a setter and tweak its behavior.
+function User(name, birthday) {
+    this.name = name;
+    this.birthday = birthday;
+  
+    // age is calculated from the current date and birthday
+    Object.defineProperty(this, "age", {
+      get() {
+        let todayYear = new Date().getFullYear();
+        return todayYear - this.birthday.getFullYear();
+      }
+    });
+  }
+  
+  let john = new User("John", new Date(1992, 6, 1));
+  
+  alert( john.birthday ); // birthday is available
+  alert( john.age );      // ...as well as the age
