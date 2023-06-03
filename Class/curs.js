7ceea83d4dc28e171a54eb6b1bd9f7f12c1785e9
Negrea-Andrei <negrea97@yahoo.com>
2023-06-03 13:07:07 +0300

    CLASS in JS

---
+++ Class/curs.js
@@ -0,0 +1,10 @@
+//!CLASS in JS
+//*In object-oriented programming, a class is an extensible program-code-template for creating objects, providing initial values for 
+//*state (member variables) and implementations of behavior (member functions or methods).
+class MyClass {
+    // class methods
+    constructor() {  }
+    method1() {  }
+    method2() {  }
+    method3() {  }    
+  }
