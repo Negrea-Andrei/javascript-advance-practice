a6d5d65bbfec47ed258ae7a6337e4848698b02a7
Negrea-Andrei <negrea97@yahoo.com>
2023-06-03 15:11:01 +0300

    Making bound methods with class fields

--- Class/curs.js
+++ Class/curs.js
@@ -22,14 +22,36 @@ class User {
 alert(typeof User); // function
 
 //!What class User {... } construct really does is:
-//*1)Creates a function named User, that becomes the result of the class declaration. The function code is taken from the constructor method(assumed empty if we don’t write such method).
-//*2)Stores class methods, such as sayHi, in User.prototype.
+//?1)Creates a function named User, that becomes the result of the class declaration. The function code is taken from the constructor method(assumed empty if we don’t write such method).
+//?2)Stores class methods, such as sayHi, in User.prototype.
 
 //!Not just a syntactic sugar!!!
-//*1)First, a function created by class is labelled by a special internal property [[IsClassConstructor]]: true. So it’s not entirely the same as creating it manually.
-//*The language checks for that property in a variety of places.
+//?1)First, a function created by class is labelled by a special internal property [[IsClassConstructor]]: true. So it’s not entirely the same as creating it manually.
+//?The language checks for that property in a variety of places.
 
-//*2)Class methods are non-enumerable. A class definition sets enumerable flag to false for all methods in the "prototype".
-//*That’s good, because if we for..in over an object, we usually don’t want its class methods.
+//?2)Class methods are non-enumerable. A class definition sets enumerable flag to false for all methods in the "prototype".
+//?That’s good, because if we for..in over an object, we usually don’t want its class methods.
 
-//*3)Classes always use strict. All code inside the class construct is automatically in strict mode.
+//?3)Classes always use strict. All code inside the class construct is automatically in strict mode.
+
+//!Making bound methods with class fields
+//*As demonstrated in the chapter Function binding functions in JavaScript have a dynamic this. It depends on the context of the call.
+//*So if an object method is passed around and called in another context, this won’t be a reference to its object any more.
+class Button {
+    constructor(value) {
+        this.value = value;
+    }
+
+    click() {
+        alert(this.value);
+    }
+}
+
+let button = new Button("hello");
+
+setTimeout(button.click, 1000); // undefined
+
+//*The problem is called "losing this".
+//*There are two approaches to fixing it, as discussed in the chapter Function binding:
+//?1)Pass a wrapper-function, such as setTimeout(() => button.click(), 1000).
+//?2)Bind the method to object, e.g. in the constructor.
