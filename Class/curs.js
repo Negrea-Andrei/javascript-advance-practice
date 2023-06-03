16e859e6b8a7c66bde85d2456a007a6e09121863
Negrea-Andrei <negrea97@yahoo.com>
2023-06-03 13:44:08 +0300

    What class does?

--- Class/curs.js
+++ Class/curs.js
@@ -13,3 +13,14 @@ class MyClass {
 
 //!What is a class?
 //*In JavaScript, a class is a kind of function.
+class User {
+    constructor(name) { this.name = name; }
+    sayHi() { alert(this.name); }
+}
+
+// proof: User is a function
+alert(typeof User); // function
+
+//!What class User {... } construct really does is:
+//*1)Creates a function named User, that becomes the result of the class declaration. The function code is taken from the constructor method(assumed empty if we don’t write such method).
+//*2)Stores class methods, such as sayHi, in User.prototype.
