e3809f7e9382041f95a6087f5de515b816d4a69d
Negrea-Andrei <negrea97@yahoo.com>
2023-06-03 13:29:54 +0300

    CLASS is a type of function in js

--- Class/curs.js
+++ Class/curs.js
@@ -3,8 +3,13 @@
 //*state (member variables) and implementations of behavior (member functions or methods).
 class MyClass {
     // class methods
-    constructor() {  }
-    method1() {  }
-    method2() {  }
-    method3() {  }    
-  }
+    constructor() { }
+    method1() { }
+    method2() { }
+    method3() { }
+}
+//*Then use new MyClass() to create a new object with all the listed methods.
+//*The constructor() method is called automatically by new, so we can initialize the object there.
+
+//!What is a class?
+//*In JavaScript, a class is a kind of function.
