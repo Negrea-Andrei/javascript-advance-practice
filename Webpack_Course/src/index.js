32f99211db20f2ddf2d38df6c63c0363250c99ee
Negrea-Andrei <negrea97@yahoo.com>
2023-06-05 20:51:28 +0300

    utput management

--- Webpack_Course/src/index.js
+++ Webpack_Course/src/index.js
@@ -1,11 +1,18 @@
 //!Webpack
 //!webpack is the go-to tool across the web for bundling and compiling JavaScript code(bundling your modules)
 import _ from 'lodash';
+import printMe from './print.js';
 
  function component() {
    const element = document.createElement('div');
+  const btn = document.createElement('button');
 
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
    return element;
  }
