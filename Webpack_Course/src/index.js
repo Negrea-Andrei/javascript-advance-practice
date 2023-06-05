8166e71b03f4a19a326389d7c49c7e11727aac81
Negrea-Andrei <negrea97@yahoo.com>
2023-06-05 19:30:56 +0300

    Work on the Webpack procesing the code during the compilation step

--- Webpack_Course/src/index.js
+++ Webpack_Course/src/index.js
@@ -1,9 +1,10 @@
 //!Webpack
 //!webpack is the go-to tool across the web for bundling and compiling JavaScript code(bundling your modules)
+import _ from 'lodash';
 function component() {
     const element = document.createElement('div');
   
-    // Lodash, currently included via a script, is required for this line to work
+    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
     return element;
