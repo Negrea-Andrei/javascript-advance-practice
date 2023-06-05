09c9abfcf7039c5d9498bf1c778254f364133068
Negrea-Andrei <negrea97@yahoo.com>
2023-06-05 20:04:52 +0300

    Loading images

--- Webpack_Course/src/index.js
+++ Webpack_Course/src/index.js
@@ -1,11 +1,13 @@
 //!Webpack
 //!webpack is the go-to tool across the web for bundling and compiling JavaScript code(bundling your modules)
 import _ from 'lodash';
+import './style.css';
 function component() {
     const element = document.createElement('div');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
   
     return element;
   }
