eded209c5f65944b82decebb20a9982cac8946ea
Negrea-Andrei <negrea97@yahoo.com>
2023-06-06 19:56:26 +0300

    Watch Mode

--- Webpack_Course/webpack.config.js
+++ Webpack_Course/webpack.config.js
@@ -1,23 +1,24 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+{
+  "name": "webpack-demo",
+  "version": "1.0.0",
+  "description": "",
+  "private": true,
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+   "watch": "webpack --watch",
+    "build": "webpack"
   },
- devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "html-webpack-plugin": "^4.5.0",
+    "webpack": "^5.4.0",
+    "webpack-cli": "^4.2.0"
   },
-};
+  "dependencies": {
+    "lodash": "^4.17.20"
+  }
+}
 //!In order to make it easier to track down errors and warnings, JavaScript offers source maps, which map your compiled code back to your original source code.
 //!If an error originates from b.js, the source map will tell you exactly that.
