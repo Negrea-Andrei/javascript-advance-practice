e91e0216447665c6fcc93151607596407432551b
Negrea-Andrei <negrea97@yahoo.com>
2023-06-19 19:32:30 +0300

    OOP: learn about principles

---
+++ OOP/OOP.js
@@ -0,0 +1,3 @@
+//!Keep in mind that there is not usually a very clear answer to your application design questions. 
+//!Some patterns and ideas are obviously better than others, but there is often some trade-off when deciding where to put a specific function. 
+//!In other words… these principles are not rules- they’re helpful guidelines.  
