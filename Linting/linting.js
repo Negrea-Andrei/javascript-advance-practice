530eefd13dd475f9629889d7e0a743b61c9aab1b
Negrea-Andrei <negrea97@yahoo.com>
2023-07-23 20:25:47 +0300

    Start the Linting part

---
+++ Linting/linting.js
@@ -0,0 +1,3 @@
+/*Before we dive all the way into the Code, we are going to take a moment to improve your editor setup. Doing this now will make things much easier for you going forward.
+This lesson will give you some information about code style, and then give you some tools to help you maintain consistent code-style throughout your projects.
+In some cases it can even help adjust things like indentation for you!*/
