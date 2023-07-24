500d34da541d325f5af362e71cae45575b5895da
Negrea-Andrei <negrea97@yahoo.com>
2023-07-24 22:29:45 +0300

    test

--- Linting/linting.js
+++ Linting/linting.js
@@ -5,3 +5,5 @@ In some cases it can even help adjust things like indentation for you!*/
 //!The style guides we mentioned above are full of really helpful advice for formatting, organizing and composing your code. But there are a lot of rules - it can be difficult to internalize them all. 
 //!Linters are tools that will scan your code with a set of style rules and will report any errors to you that they find. In some cases, they can even auto-fix the errors! 
 //!The following articles explain in more detail the benefits of using a linter while you code.
+
+//Test
