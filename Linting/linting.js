5c13a66b13b801e68222f50dec670b24720ed0f3
Negrea-Andrei <negrea97@yahoo.com>
2023-07-24 19:48:42 +0300

    explain what linters are

--- Linting/linting.js
+++ Linting/linting.js
@@ -1,3 +1,7 @@
 /*Before we dive all the way into the Code, we are going to take a moment to improve your editor setup. Doing this now will make things much easier for you going forward.
 This lesson will give you some information about code style, and then give you some tools to help you maintain consistent code-style throughout your projects.
 In some cases it can even help adjust things like indentation for you!*/
+
+//!The style guides we mentioned above are full of really helpful advice for formatting, organizing and composing your code. But there are a lot of rules - it can be difficult to internalize them all. 
+//!Linters are tools that will scan your code with a set of style rules and will report any errors to you that they find. In some cases, they can even auto-fix the errors! 
+//!The following articles explain in more detail the benefits of using a linter while you code.
