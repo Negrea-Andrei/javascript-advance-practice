c36e33cbdb96caba0f0f75a542cfb874d4177617
Negrea-Andrei <negrea97@yahoo.com>
2023-07-27 20:15:22 +0300

    JSON structure

--- JSON/JSON.js
+++ JSON/JSON.js
@@ -1,2 +1,6 @@
 //!JSON (JavaScript Object Notation) is a standardized format for structuring data. It is heavily based on the syntax for JavaScript objects.
 //!It is commonly used for transmitting data in web applications
+
+//*Even though it closely resembles JavaScript object literal syntax, it can be used independently from JavaScript, and many programming environments feature the ability to read (parse) and generate JSON.
+
+//!JSON structure
