f5411f67fd13f472e3def7035a7ba28240d83f9a
Negrea-Andrei <negrea97@yahoo.com>
2023-07-29 19:53:51 +0300

    exemplu

--- JSON/JSON.js
+++ JSON/JSON.js
@@ -23,3 +23,8 @@ const getData = function() {
     // clean it up a bit and return it as an object:
     return data
   }
+
+//*The issue with this example is that it takes some time to fetch the data, but unless we tell our code that, it assumes that everything in the function happens essentially instantly.
+
+const myData = getData()
+const pieceOfData = myData['whatever']
