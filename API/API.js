81fb62c538b87e1f26e07e2a66fbec35a496bcff
Negrea-Andrei <negrea97@yahoo.com>
2023-07-31 19:36:49 +0300

    Expalin how API work

--- API/API.js
+++ API/API.js
@@ -1,1 +1,7 @@
 //*Servers that are created for serving data for external use (in websites or apps) are often referred to as APIs or ‘Application Programming Interfaces’.
+
+/*There are multiple ways of requesting data from an API, but all of them basically do the same thing. For the most part, 
+APIs are accessed through URLs, and the specifics of how to query these URLs change based on the specific service you are using. 
+For example, WeatherAPI has several types of data that you can request. To get the current weather in a specific location, 
+you can pass in the name of a city (optionally, you can also pass a zip code & even an ip-address!) as a URL query string parameter*/
+
