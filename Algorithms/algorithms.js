c7d8820ed59ba55cb5ddba968567b57a8510fb6b
Negrea-Andrei <negrea97@yahoo.com>
2023-08-10 19:59:34 +0300

    what is recursion?

---
+++ Algorithms/algorithms.js
@@ -0,0 +1,4 @@
+//!Recursion is the idea that a function calls itself. 
+/*Pretty simple, right? It’s used to take a big problem and start breaking it down into smaller and smaller pieces
+ and continuing to feed their solutions back into the original function until some sort of answer is achieved and the whole chain unwinds.*/
+ 
