425df05b60b0ed143f098ef88a5018c0538bacfe
Negrea-Andrei <negrea97@yahoo.com>
2023-08-16 19:30:51 +0300

    calculate the number of steps

--- time complexity/time_complexity.js
+++ time complexity/time_complexity.js
@@ -10,3 +10,15 @@ function oddNumbersLessThanTen() {
     currentNumber += 1;
   }
 }
+/*
+1)We assign the number 1 to a variable. That’s one step.
+
+1) We have a loop. For each iteration of the loop, we do the following:
+  1) Compare currentNumber to see if it is less than 10. That is 1 step.
+  2) We then check if currentNumber is odd. That is 1 step.
+  3) If it is then we output it to the terminal. That’s 1 step every 2 iterations.
+  4) We increase currentNumber by 1. That is 1 step.
+  //*27steps + 5 steps
+
+3) To exit the loop, we need to compare currentNumber one last time to see that it is not less than ten any more. That is one last step.*/
+/*27 + 5 + 1 +1 = 34*/
