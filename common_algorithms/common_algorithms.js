cdc9f6ca61a9daa4a3a6d518272bb3739faf5e72
Negrea-Andrei <negrea97@yahoo.com>
2023-08-21 19:54:56 +0300

    What are data structures used for?

--- common_algorithms/common_algorithms.js
+++ common_algorithms/common_algorithms.js
@@ -1,2 +1,9 @@
 //!Data structure is a data organization, management, and storage format that is usually chosen for efficient access to data. More precisely, 
 //!a data structure is a collection of data values, the relationships among them, and the functions or operations that can be applied to the data
+
+/*Different types of data structures are suited to different kinds of applications, and some are highly specialized to specific tasks.*/
+
+/*Data structures provide a means to manage large amounts of data efficiently for uses such as large databases and internet indexing services. 
+Usually, efficient data structures are key to designing efficient algorithms. 
+Some formal design methods and programming languages emphasize data structures, rather than algorithms, as the key organizing factor in software design. 
+Data structures can be used to organize the storage and retrieval of information stored in both main memory and secondary memory*/
