3fe1a7d92fb0e4bace9ed536faa9c09eccca343d
Negrea-Andrei <negrea97@yahoo.com>
2023-08-29 20:06:07 +0300

    commit

--- git_advance/git_advance.js
+++ git_advance/git_advance.js
@@ -2,4 +2,13 @@
 
 
 //*Changing history
-/*
+
+//? to change the most recent commit you can use:
+//!git commit --amend
+
+//?change more commits you can use:
+//! git rebase -i HEAD~<the number of commits>
+//*the VS code will open, change the PICK to edit and add the changes
+
+//?Squashing commits
+//!
