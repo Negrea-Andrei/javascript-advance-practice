95624617b6f346ff9e38546cf1315572790036f0
Negrea-Andrei <negrea97@yahoo.com>
2023-08-29 20:12:19 +0300

    learn more git commands

--- git_advance/git_advance.js
+++ git_advance/git_advance.js
@@ -11,4 +11,11 @@
 //*the VS code will open, change the PICK to edit and add the changes
 
 //?Squashing commits
-//!
+//!select the 2 commit you want to unite
+//!on the second one use squash and add the description
+
+//?to force a push:
+//!git push --force
+
+//?to revert a commit:
+//!git revert
