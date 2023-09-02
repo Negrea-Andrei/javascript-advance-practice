2f9e1c91f4009a036200048e2216d8572db99932
Negrea-Andrei <negrea97@yahoo.com>
2023-09-02 17:13:21 +0300

    The proprieties of transform

--- html_css_advance/html_css_advance.js
+++ html_css_advance/html_css_advance.js
@@ -2,7 +2,7 @@
 
 //!The transform property takes in one or more CSS transform functions as its values, with those functions taking in their own value, usually an angle or a number.
 
-//?
-//?
-//?
-//?
+//?rotate
+//?scale
+//?skew
+//?translate
