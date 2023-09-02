bebb066192cc854e3ad56a59e389276a1ab1dd65
Negrea-Andrei <negrea97@yahoo.com>
2023-09-02 20:01:18 +0300

    animations

--- html_css_advance/html_css_advance.js
+++ html_css_advance/html_css_advance.js
@@ -19,3 +19,17 @@
 //?transition-delay: how long until the transition start
 
 //*instead of all of this you could use just "transition"
+
+//!Keyframes
+//!Animations
+
+/*Transitions were designed to animate an element from one state to another. 
+They can loop, but they weren’t designed for that. Animations, on the other hand, were designed with the purpose of explicitly enabling loops.*/
+
+/*Transitions need a trigger, such as the use of pseudo-classes like :hover or :focus, or by adding/removing a class via JavaScript. 
+Animations, on the other hand, do not need such a trigger. Once you have your elements in place and CSS defined, 
+an animation will start running immediately if that’s what you told it to do.*/
+
+/*Transitions are not as flexible as using animations. When you define a transition, imagine you are sending that element on a journey 
+in a straight line from point A to point B. Yes, the transition-timing-function can add some variation to the timing of this change, 
+but it doesn’t compare to the amount of flexibility added by using animations.*/
