e5d8e97b7175605a1c70ef3f9b792ae1a470328d
Negrea-Andrei <negrea97@yahoo.com>
2023-09-03 16:06:47 +0300

    heading and landmarks

---
+++ html_css_advance/html_semantic.js
@@ -0,0 +1,13 @@
+//!Headings and landmarks
+//*Headings are the <h1> through <h6> elements, and like the name implies, these elements act as headings to sections of a page. 
+//*Landmarks, on the other hand, are HTML elements that act as regions of a page. There are seven native HTML elements that define these landmark regions:
+
+//?
+/*
+<aside>
+<footer>
+<form>
+<header>
+<main>
+<nav>
+<section></section> */
