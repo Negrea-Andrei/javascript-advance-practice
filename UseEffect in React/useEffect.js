66795bbd4ef908b81fcd0da504771a61c81806b2
Negrea-Andrei <negrea97@yahoo.com>
2023-11-01 19:11:23 +0200

    the correct way to use side effect

--- UseEffect in React/useEffect.js
+++ UseEffect in React/useEffect.js
@@ -15,3 +15,24 @@ export default function Clock() {
 
 //!XXXXXXXXXXX
 
+import React, { useEffect, useState } from "react";
+
+export default function Clock() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const key = setInterval(() => {
+      setCounter(count => count + 1)
+    }, 1000);
+
+    return () => {
+      clearInterval(key);
+    }
+  }, [])
+
+  return (
+    <p>{counter} seconds have passed.</p>
+  );
+}
+
+//*OOOOOOOOOOOOOOOOOOOO
