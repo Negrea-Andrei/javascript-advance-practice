d6a6c1307fe5eab86b4298d41e8cc321d9c72075
Negrea-Andrei <negrea97@yahoo.com>
2023-11-18 21:18:42 +0200

    Components life circle

--- UseEffect in React/useEffect.js
+++ UseEffect in React/useEffect.js
@@ -36,3 +36,25 @@ export default function Clock() {
 }
 
 //*OOOOOOOOOOOOOOOOOOOO
+
+//*Component Lifecycle Methods
+
+
+//!componentDidMount()
+//?This method is run after the component is mounted (inserted in the DOM tree). You should make any calls to fetch data that is needed for the component here. It is also a good place to do anything that is reliant on the component, such as drawing on a canvas element that you just rendered.
+
+//!componentDidUpdate()
+//?This method is run after a component re-renders. Because of this, you have to be careful about what you update in this method, as if you’re updating state indiscriminately, a re-render is caused, and you’ll end up in an endless loop. You can avoid this issue by using conditional statements about the equality of previous and current props when updating state.
+
+//?In this method you should be updating anything that needs to be changed in response to either the DOM changing, or any states that you might want to act on upon change. For example, you’d want to refetch user data if the user changes.
+
+//!componentWillUnmount()
+//?This is the last lifecycle method, which is called before a component is unmounted and destroyed. In this method you should be performing cleanup actions, so that would be cancelling network requests, clearing timers, etc.
+
+//*How useEffect() combines the lifecycle methods
+//?Now that we’ve learnt about class lifecycle methods, it’s useful to understand that the useEffect hook used in functional components is essentially a combination of componentDidMount, componentDidUpdate, and componentWillUnmount. Which method/methods it relates to varies based on it’s dependency array, and if it returns anything.
+
+/*An empty dependency array would be equivalent to componentDidMount.
+A dependency array with a value/values in it would be a combination of componentDidMount and componentDidUpdate, but only updating when dependencies change.
+No dependency array would be equivalent to componentDidMount and componentDidUpdate combined.
+A return function inside of a useEffect() hook would be equivalent to componentWillUnmount.*/
