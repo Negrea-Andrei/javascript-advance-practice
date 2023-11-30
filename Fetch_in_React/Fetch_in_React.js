0e0672cf7f48d9df15ac7cc0bfbb7b1d1642adc0
Negrea-Andrei <negrea97@yahoo.com>
2023-11-30 19:58:52 +0200

    add in case of error

--- Fetch_in_React/Fetch_in_React.js
+++ Fetch_in_React/Fetch_in_React.js
@@ -8,23 +8,25 @@
 import { useEffect, useState } from "react";
 
 const Image = () => {
-  const [imageURL, setImageURL] = useState(null);
+    const [imageURL, setImageURL] = useState(null);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
-      .then((response) => response.json())
-      .then((response) => setImageURL(response[0].url))
-      .catch((error) => console.error(error));
-  }, []);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
+            .then((response) => response.json())
+            .then((response) => setImageURL(response[0].url))
+            .catch((error) => console.error(error));
+    }, []);
 
-  return (
-    imageURL && (
-      <>
-        <h1>An image</h1>
-        <img src={imageURL} alt={"placeholder text"} />
-      </>
-    )
-  );
-};
+    if (error) return <p>A network error was encountered</p>
+
+    return (
+        imageURL && (
+            <>
+                <h1>An image</h1>
+                <img src={imageURL} alt={"placeholder text"} />
+            </>
+        )
+    );
+}
 
 export default Image;
