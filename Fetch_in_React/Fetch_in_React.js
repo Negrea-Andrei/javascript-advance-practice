46e533b5c1a0d78171ba7257291398cffd588dcb
Negrea-Andrei <negrea97@yahoo.com>
2023-12-01 22:00:18 +0200

    My birthday was today. Took a break. Will resume tomorrow.

--- Fetch_in_React/Fetch_in_React.js
+++ Fetch_in_React/Fetch_in_React.js
@@ -8,32 +8,34 @@
 import { useEffect, useState } from "react";
 
 const Image = () => {
-    const [imageURL, setImageURL] = useState(null);
-    /*need to add a state for the error */
-    const [error, setError] = useState(null);
+  const [imageURL, setImageURL] = useState(null);
+  /*need to add a state for the error */
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
-          .then((response) => {
-            if (response.status >= 400) {
-              throw new Error("server error");
-            }
-            return response.json();
-          })
-          .then((response) => setImageURL(response[0].url))
-          .catch((error) => setError(error));
-      }, []);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => setImageURL(response[0].url))
+      .catch((error) => setError(error));
+  }, []);
 
-    if (error) return <p>A network error was encountered</p>
+  if (error) return <p>A network error was encountered</p>
 
-    return (
-        imageURL && (
-            <>
-                <h1>An image</h1>
-                <img src={imageURL} alt={"placeholder text"} />
-            </>
-        )
-    );
+  return (
+    imageURL && (
+      <>
+        <h1>An image</h1>
+        <img src={imageURL} alt={"placeholder text"} />
+      </>
+    )
+  );
 }
 
 export default Image;
+
+//*My birthday was today. Took a break. Will resume tomorrow.
