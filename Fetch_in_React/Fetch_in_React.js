058fbe4781b0395437146200bd9c4e754f8556b4
Negrea-Andrei <negrea97@yahoo.com>
2023-12-03 19:16:59 +0200

    Custom hooks logic

--- Fetch_in_React/Fetch_in_React.js
+++ Fetch_in_React/Fetch_in_React.js
@@ -47,3 +47,40 @@ Add condition if(error) return <p>Error</p>
               if(loading) return <p>Loading</p>*/
 
 //*Custom hooks
+/*separate out the fetching logic altogether into a custom hook. This will allow us to make the logic reusable and easily testable*/
+import { useState, useEffect } from "react";
+
+const useImageURL = () => {
+  const [imageURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => setImageURL(response[0].url))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { imageURL, error, loading };
+};
+
+const Images = () => {
+  const { imageURL, error, loading } = useImageURL();
+
+  if (error) return <p>A network error was encountered</p>;
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <h1>An image</h1>
+      <img src={imageURL} alt={"placeholder text"} />
+    </>
+  );
+};
